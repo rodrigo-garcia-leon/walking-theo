3f940021b25255140be7b88e7e668323ee6c2e5e
Rodrigo García <yo@rodrigogarcia.me>
2020-05-30 21:14:46 +0200

    Add scene switching

--- scripts/theo.gd
+++ scripts/theo.gd
@@ -1,13 +1,27 @@
 extends KinematicBody2D
 
+var velocity
 var speed = 100
-var jumpTime = 0.2
 
-var velocity = Vector2()
 var isJumping = false
+var jumpTime = 0.2
 var jumpCounter = jumpTime
 
+var changeSceneTime = 1
+var changeSceneCounter = changeSceneTime
+
+var currentScene
+var nextScene
+
 func _physics_process(delta):
+	if changeSceneCounter > 0:
+		changeSceneCounter -= delta
+
+	if Input.is_action_pressed('ui_accept') and changeSceneCounter < 0:
+		currentScene = get_tree().current_scene.name
+		nextScene = 'res://scenes/second.tscn' if currentScene != 'second' else 'res://scenes/first.tscn'
+		get_tree().change_scene(nextScene)
+
 	velocity = Vector2()
 
 	if Input.is_action_pressed('ui_up') and is_on_floor():
